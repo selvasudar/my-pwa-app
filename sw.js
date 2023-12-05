44eaad9bab5f52d5d3efbf35353949625d647d42
Selvakumar D <selvasudar3@gmail.com>
2023-12-05 22:10:35 +0530

    timing based cache added

--- sw.js
+++ sw.js
@@ -7,6 +7,8 @@ const filesToCache = [
     '/script.js'
 ];
 
+const cacheDurationInMinutes = 60; // Cache duration in minutes
+
 self.addEventListener('install', (e) => {
     e.waitUntil(
         caches.open(cacheName).then((cache) => {
@@ -33,6 +35,28 @@ self.addEventListener('fetch', (e) => {
     e.respondWith(
         caches.match(e.request).then((response) => {
             return response || fetch(e.request);
+        })
+    );
+});
+
+// Check for cache expiration and update cache
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.open(cacheName).then((cache) => {
+            return cache.match(e.request).then((response) => {
+                const fetchPromise = fetch(e.request).then((networkResponse) => {
+                    cache.put(e.request, networkResponse.clone());
+                    return networkResponse;
+                });
+
+                // Check if cache is older than cacheDurationInMinutes
+                const cacheDate = response ? new Date(response.headers.get('date')) : null;
+                if (cacheDate && (Date.now() - cacheDate.getTime()) > (cacheDurationInMinutes * 60 * 1000)) {
+                    return fetchPromise;
+                } else {
+                    return response || fetchPromise;
+                }
+            });
         })
     );
 });
